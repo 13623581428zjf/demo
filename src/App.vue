3cdca8bb8dccbc848838fdb385380cb1abb31928
13623581428zjf <13623581428@163.com>
2020-11-25 15:00:24 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+    body{
+    	height: 100%;
+    	margin: 0;
+    	padding: 0;
+    }
+    ul,li{
+    	text-decoration: none;
+    }
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,206 @@
+<template>
+	<div>
+		<!--logo-->
+		<a href="lianxi.vue">点我跳练习</a> 
+		<div class="banner">
+			<!--俩点-->
+			<div class="diner">
+				<a href="/" class="clod1"></a>
+				<a href="/" class="clod2"></a>
+			</div>
+		</div>
+		<!--2020年热门网页游戏>>>-->
+		<div class="banner-box">
+			<div class="jy-box-tit-1">
+				<span class="jy-tit-fs">3333333</span>
+			</div>
+		</div>
+		<!--人气-->
+		<div class="hot-b-game">
+			<div class="hot-b-game-one">
+				<div class="gm">
+					<div class='game1'>真龙主宰</div>
+					<!--<div class='game2'>2020超爽高爆单职业</div>-->
+					<div class='game3'>开始游戏</div>
+				</div>
+			</div>
+			<div class="hot-b-game-one">
+				<div class="gm">
+					<div class='game1'>真龙主宰</div>
+					<!--<div class='game2'>2020超爽高爆单职业</div>-->
+					<div class='game3'>开始游戏</div>
+				</div>
+			</div>
+			<div class="hot-b-game-one">
+				<div class="gm">
+					<div class='game1'>真龙主宰</div>
+					<!--<div class='game2'>2020超爽高爆单职业</div>-->
+					<div class='game3'>开始游戏</div>
+				</div>
+			</div>
+			<div class="hot-b-game-one">
+				<div class="gm">
+					<div class='game1'>真龙主宰</div>
+					<!--<div class='game2'>2020超爽高爆单职业</div>-->
+					<div class='game3'>开始游戏</div>
+				</div>
+			</div>
+			<div class="hot-b-game-one">
+				<div class="gm">
+					<div class='game1'>真龙主宰</div>
+					<!--<div class='game2'>2020超爽高爆单职业</div>-->
+					<div class='game3'>开始游戏</div>
+				</div>
+			</div>
+		</div>
+		<!--游戏排行-->
+		<div class="gremp">
+			<div class="grempl">
+				<div class="grempft"> 11111111</div>
+			</div>
+			<div class="grempr">
+				<div class="gremprt">22222222222</div>
+			</div>
+		</div>
+		<!--真龙主宰-->
+		<div class="jy-lt">
+			<div class="jy-lt-float">
+				<div class="jy-lt-floatBox">
+					<span class="jy-lt-num" style="list-style: none;">1</span>
+					<span class="jy-it-font">真龙主宰</span>
+					<span class="jy-img"><img src="../assets/i-s-praise.gif"/></span>
+				</div>
+				<div class="y-lt-floatimg">
+					<div class="y-le">
+						<img class="y-lt-floatimgs" src="../assets/l1.gif" />
+					</div>
+					<div class="y-lere">
+						<span>真龙主宰</span>
+						<span class="tweo">上线VIP12</span>
+						<span>开始游戏</span>
+					</div>
+				</div>
+				<div class="jy-lt-floatBox">
+					<span class="jy-lt-num" style="list-style: none;">1</span>
+					<span class="jy-it-font">真龙主宰</span>
+					<span class="jy-img"><img src="../assets/i-s-praise.gif"/></span>
+				</div>
+				<div class="jy-lt-floatBox">
+					<span class="jy-lt-num" style="list-style: none;">1</span>
+					<span class="jy-it-font">真龙主宰</span>
+					<span class="jy-img"><img src="../assets/i-s-praise.gif"/></span>
+				</div>
+				<div class="jy-lt-floatBox">
+					<span class="jy-lt-num" style="list-style: none;">1</span>
+					<span class="jy-it-font">真龙主宰</span>
+					<span class="jy-img"><img src="../assets/i-s-praise.gif"/></span>
+				</div>
+				<div class="jy-lt-floatBox">
+					<span class="jy-lt-num" style="list-style: none;">1</span>
+					<span class="jy-it-font">真龙主宰</span>
+					<span class="jy-img"><img src="../assets/i-s-praise.gif"/></span>
+				</div>
+				<div class="huo">
+					44
+				</div>
+				<div style="border-bottom: 1px solid #a1c6e1;border-left:1px solid #a1c6e1 ;border-right: 1px solid #a1c6e1;margin-top: -10px;">
+					<div class="hbfw"><img src="../assets/i-s-hot.gif" />
+						<p> 真龙主宰&nbsp;&nbsp;&nbsp;&nbsp;高爆夫<span>进入>></span></p>
+					</div>
+					<div class="hbfw"><img src="../assets/i-s-hot.gif" />
+						<p> 真龙主宰&nbsp;&nbsp;&nbsp;&nbsp;高爆夫<span>进入>></span></p>
+					</div>
+					<div class="hbfw"><img src="../assets/i-s-hot.gif" />
+						<p> 真龙主宰&nbsp;&nbsp;&nbsp;&nbsp;高爆夫<span>进入>></span></p>
+					</div>
+					<div class="hbfw"><img src="../assets/i-s-hot.gif" />
+						<p> 真龙主宰&nbsp;&nbsp;&nbsp;&nbsp;高爆夫<span>进入>></span></p>
+					</div>
+					<div class="hbfw"><img src="../assets/i-s-hot.gif" />
+						<p> 真龙主宰&nbsp;&nbsp;&nbsp;&nbsp;高爆夫<span>进入>></span></p>
+					</div>
+				</div>
+			</div>
+			<div class="jy-lt-right">
+				<div class="jy-lt-rightpic">
+					<div class="jy-lt-rightpicLEFt">
+						<img src="../assets/tyty.gif" />
+						<p class="jy-font">
+							<span>真龙主宰</span>
+							<span style="color: #777;font-size: 12px;margin-top: 5px;">类型：即时战斗</span>
+							<span style="color: #777;font-size: 12px;">游戏简介：切割单职业，一道破万亿，高价回收。</span>
+							<span>进入游戏</span>
+						</p>
+					</div>
+					<div class="jy-lt-rightpicright">
+						<img src="../assets/tyty.gif" />
+						<p class="jy-font">
+							<span>真龙主宰</span>
+							<span style="color: #777;font-size: 12px;margin-top: 5px;">类型：即时战斗</span>
+							<span style="color: #777;font-size: 12px;">游戏简介：切割单职业，一道破万亿，高价回收。</span>
+							<span>进入游戏</span>
+						</p>
+					</div>
+				</div>
+				<div class="jy-lt-rightpic">
+					<div class="jy-lt-rightpicLEFt">
+						<img src="../assets/tyty.gif" />
+						<p class="jy-font">
+							<span>真龙主宰</span>
+							<span style="color: #777;font-size: 12px;margin-top: 5px;">类型：即时战斗</span>
+							<span style="color: #777;font-size: 12px;">游戏简介：切割单职业，一道破万亿，高价回收。</span>
+							<span>进入游戏</span>
+						</p>
+					</div>
+					<div class="jy-lt-rightpicright">
+						<img src="../assets/tyty.gif" />
+						<p class="jy-font">
+							<span>真龙主宰</span>
+							<span style="color: #777;font-size: 12px;margin-top: 5px;">类型：即时战斗</span>
+							<span style="color: #777;font-size: 12px;">游戏简介：切割单职业，一道破万亿，高价回收。</span>
+							<span>进入游戏</span>
+						</p>
+					</div>
+				</div>
+				<div class="jy-lt-rightpic">
+					<div class="jy-lt-rightpicLEFt">
+						<img src="../assets/tyty.gif" />
+						<p class="jy-font">
+							<span>真龙主宰</span>
+							<span style="color: #777;font-size: 12px;margin-top: 5px;">类型：即时战斗</span>
+							<span style="color: #777;font-size: 12px;">游戏简介：切割单职业，一道破万亿，高价回收。</span>
+							<span>进入游戏</span>
+						</p>
+					</div>
+					<div class="jy-lt-rightpicright">
+						<img src="../assets/tyty.gif" />
+						<p class="jy-font">
+							<span>真龙主宰</span>
+							<span style="color: #777;font-size: 12px;margin-top: 5px;">类型：即时战斗</span>
+							<span style="color: #777;font-size: 12px;">游戏简介：切割单职业，一道破万亿，高价回收。</span>
+							<span>进入游戏</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--清除浮动-->
+		<div class="clearfix"></div>
+		<div class="foot">
+			<p>江西贪玩信息技术有限公司 赣网文[2017]2191-007号 赣ICP备16012630号-4 江西省上饶市上饶县凤凰西大道76号阳光时代写字楼1幢2501-2508室</p>
+			<p>广告仅供参考，请以游戏为准</p>
+			<img src="../assets/wenhuajingying.png" />
+		</div>
+	</div>
+
+</template>
+<script src="../assets/scc/home.css"></script>
+<script>
+	export default {
+		 
+          
+	}
+</script>
+<style scoped>
+	@import url("../assets/css/home.css");
+</style>
